cb5efd61ff4fab446968cfc813301cbcc4279806
Jeremyah Payne <thefluentone@gmail.com>
2023-06-18 23:29:36 -0500

    Initial Commit

---
+++ LinearTimer Watch App/LinearTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinearTimerApp.swift
+//  LinearTimer Watch App
+//
+//  Created by Jeremyah Payne on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LinearTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
